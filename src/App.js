29e2a253f320ccc3fa9d11c6839eb1e716adc17a
RedPandarRevolt <redpandarevolt@gmail.com>
2020-12-04 21:31:10 -0800

    Iniated user log in data to go into firebase database for project.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ShopPage from './pages/shop/shop.component';
 
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth} from './firebase/firebase.utils';
+import { auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 class App extends React.Component {
     constructor(props) {
@@ -21,12 +21,9 @@ class App extends React.Component {
 
     unsubscribeFromAuth = null
 
-
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged( user => {
-            this.setState({ currentUser: user});
-
-            console.log(user);
+        this.unsubscribeFromAuth = auth.onAuthStateChanged( async user => {
+            createUserProfileDocument(user);
         });
     }
 
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -54,7 +54,7 @@ class SignIn extends React.Component {
                     <div className='buttons'>
                         <CustomButton type='submit' >Sign In</CustomButton>
                         <CustomButton onClick={signInWithGoogle} isGoogleSignIn >
-                            Sign in with Google
+                            SIGNIN WITH GOOGLE
                         </CustomButton>
                     </div>
                 </form>
